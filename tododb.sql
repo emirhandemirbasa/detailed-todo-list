a08a4e86dffc9d4a17c0c151ed0e3cf298392470
Emirhan Demirbaşa <58663784+emirhandemirbasa@users.noreply.github.com>
2025-07-19 14:40:32 +0300

    Veri tabanı
    Veri tabanı tabloları güncellendi ve eklendi.
    hesaplar
    hesap_detay
    notlar
    cinsiyetler
    tablolari bulunmaktadir.

--- tododb.sql
+++ tododb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 16 Tem 2025, 21:33:25
+-- Üretim Zamanı: 19 Tem 2025, 13:27:13
 -- Sunucu sürümü: 10.4.32-MariaDB
 -- PHP Sürümü: 8.2.12
 
@@ -24,6 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tablo için tablo yapısı `cinsiyetler`
+--
+
+CREATE TABLE `cinsiyetler` (
+  `id` int(11) NOT NULL,
+  `cinsiyet_adi` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Tablo döküm verisi `cinsiyetler`
+--
+
+INSERT INTO `cinsiyetler` (`id`, `cinsiyet_adi`) VALUES
+(1, 'Erkek'),
+(2, 'Kadın');
+
+-- --------------------------------------------------------
+
+--
 -- Tablo için tablo yapısı `hesaplar`
 --
 
@@ -36,16 +55,45 @@ CREATE TABLE `hesaplar` (
   `last_login_date` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Tablo döküm verisi `hesaplar`
+-- Tablo için tablo yapısı `hesap_detay`
 --
 
-INSERT INTO `hesaplar` (`id`, `username`, `email`, `password`, `register_date`, `last_login_date`) VALUES
-(1, 'Strong', 'emirhanlife@gmail.com', '$2y$10$wlQ0lnCPvnNlR/jAt0yjy.zCWI78OctLjI.UGd2CsVJz/bBB5qLlK', '2025-07-16 21:53:56', '16.07.2025');
+CREATE TABLE `hesap_detay` (
+  `id` int(11) NOT NULL,
+  `isim` varchar(25) NOT NULL,
+  `soyisim` varchar(25) NOT NULL,
+  `profil_url` varchar(255) NOT NULL,
+  `cinsiyet` int(11) NOT NULL,
+  `hesap_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `notlar`
+--
+
+CREATE TABLE `notlar` (
+  `id` int(11) NOT NULL,
+  `not_baslik` varchar(150) NOT NULL,
+  `not_detay` mediumtext NOT NULL,
+  `not_renk` varchar(20) NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `not_sahip_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dökümü yapılmış tablolar için indeksler
 --
+
+--
+-- Tablo için indeksler `cinsiyetler`
+--
+ALTER TABLE `cinsiyetler`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Tablo için indeksler `hesaplar`
@@ -54,14 +102,44 @@ ALTER TABLE `hesaplar`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Tablo için indeksler `hesap_detay`
+--
+ALTER TABLE `hesap_detay`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `notlar`
+--
+ALTER TABLE `notlar`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
 --
+
+--
+-- Tablo için AUTO_INCREMENT değeri `cinsiyetler`
+--
+ALTER TABLE `cinsiyetler`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `hesaplar`
 --
 ALTER TABLE `hesaplar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `hesap_detay`
+--
+ALTER TABLE `hesap_detay`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `notlar`
+--
+ALTER TABLE `notlar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
