4dddbed3433d0ac3f1066b87d37a23f5236abc43
Emirhan Demirbaşa <58663784+emirhandemirbasa@users.noreply.github.com>
2025-07-24 13:15:53 +0300

    Animasyon eklenmesi
    animate.css kütüphanesinin projeye eklenmesi ve arayüzlerin daha estetik ve daha hoş görünmesi sağlandı.

--- partials/_header.php
+++ partials/_header.php
@@ -9,6 +9,8 @@ if (realpath($_SERVER["SCRIPT_FILENAME"]) === realpath(__FILE__))
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anasayfa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+   
     <style>
         .bg-dgreen{
             background-color: rgba(38, 147, 69, 1);
